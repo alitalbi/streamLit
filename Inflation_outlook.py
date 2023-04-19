e6b86557d696db52759fa2c429bb57725d5d600d
Ali <ali.talbi@etu.univ-paris1.fr>
2023-04-19 18:12:56 +0200

    adding new page monetary policy

---
+++ Inflation_outlook.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Inflation Outlook",page_icon="📈")
+# Set title and description of the app
+st.title("Inflation Outlook")
+st.write("Inflation Outlook ")
---
+++ Nowcasting_indicators.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Nowcasting Indicators",page_icon="📈")
+# Set title and description of the app
+st.title("Nowcasting Indicators")
+st.write("Nowcasting Indicators")
