1f63ac73b95422af5be80b408bcea2a7eb25bc7e
Ali <ali.talbi@etu.univ-paris1.fr>
2023-04-19 18:27:01 +0200

    adding new pages monetary policy

--- Monertary_policy.py
+++ Monertary_policy.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 
-st.set_page_config(page_title="DataFrame Demo", page_icon="📊")
+st.set_page_config(page_title="DataFrame Demo")
 
 st.markdown("# DataFrame Demo")
 st.sidebar.header("DataFrame Demo")
--- growth.py
+++ growth.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.set_page_config(page_title="growth",page_icon="📈")
+st.set_page_config(page_title="growth")
 # Set title and description of the app
 st.markdown("growth")
 st.sidebar.header("growth")
