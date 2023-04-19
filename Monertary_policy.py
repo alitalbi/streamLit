aa0a209be88241b1d1fec074bcaaa2e08512deea
Ali <ali.talbi@etu.univ-paris1.fr>
2023-04-19 18:11:45 +0200

    added path to site-packages to include y finance

---
+++ Monertary_policy.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Monetary Policy",page_icon="📈")
+# Set title and description of the app
+st.title("Monetary Policy")
+st.write("Monetary Policy ")
