2b723470b42969154e89fa0da19d071ba532907d
alitalbi <34696044+alitalbi@users.noreply.github.com>
2024-11-29 02:41:52 +0100

    Update Risk_on_off.py

--- pages/Risk_on_off.py
+++ pages/Risk_on_off.py
@@ -20,7 +20,7 @@ def momentum(period,etf1,etf2):
         yf.download(etf1, date_start, date_end, period=frequency))
     data_etf2 = pd.DataFrame(
         yf.download(etf2, date_start, date_end, period=frequency))
-
+    st.write(data_etf1)
     ratio_etfs = data_etf1['Close'][etf1]/data_etf2['Close'][etf2]
   
     if period == "1w":
