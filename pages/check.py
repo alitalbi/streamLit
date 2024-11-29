bd1a31f1cde276c0a41e235ce3356913ec626f1b
alitalbi <34696044+alitalbi@users.noreply.github.com>
2024-11-29 02:40:35 +0100

    Delete pages/check.py

--- pages/check.py
+++
@@ -1,5 +0,0 @@
-import requests
-import pandas as pd
-cli = pd.read_csv("https://sdmx.oecd.org/public/rest/data/OECD.SDD.STES,DSD_STES@DF_CLI,/.M.LI...AA...H?startPeriod=2024-02&dimensionAtObservation=AllDimensions&format=csvfilewithlabels")
-cli = cli.loc[cli["REF_AREA"]=="USA"][["TIME_PERIOD","OBS_VALUE"]]
-print(cli)
