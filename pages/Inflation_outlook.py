a00a495c831717ba854a259e609ded86d7e6a5b3
alitalbi <34696044+alitalbi@users.noreply.github.com>
2024-12-10 03:49:09 +0100

    Update Inflation_outlook.py

--- pages/Inflation_outlook.py
+++ pages/Inflation_outlook.py
@@ -8,7 +8,7 @@ import yfinance as yf
 from datetime import datetime,timedelta
 import os
 from pandas.tseries.offsets import BDay
-st.set_page_config(page_title="Inflation",layout="wide",initial_sidebar_state="collapsed")
+st.set_page_config(page_title="Inflation",layout="wide")
 
 
 frequency = "monthly"
